9f10b731a9235f250cb5f5d063a9e3d915aabf4c
Oloche Richard <richardadakoleoloche@gmail.com>
2022-12-28 22:45:02 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View , Button } from "react-native";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 function App() {
   return (
     <View style={styles.container}>
-    <View> Yet another text </View>
-      <Text style={{margin:16, borderWidth:2 , borderColor:'blue' , padding: 16}}>
-      Hello World</Text>
-      <Button title='Tap me'/>
+      <View>
+        <TextInput />
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-  backgroundColor: '#fff',
-  alignItems:'center',
-  justifyContent:'center'
-  }
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default App;
